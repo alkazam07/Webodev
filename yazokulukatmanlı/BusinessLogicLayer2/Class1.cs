e7a71c876cedb1c386113cb0d37333dbe1f74931
alkazam07 <85650031+alkazam07@users.noreply.github.com>
2021-06-10 00:50:52 +0300

    Add files via upload

---
+++ yazokulukatmanlı/BusinessLogicLayer2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayerr;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    class BLLOgrenci
+    {
+        public static int OgrenciEkleBLL(EntityOgrenci p)
+        {
+            if (p.AD != null && p.SOYAD != null && p.NUMARA != null && p.SIFRE != null && p.FOTOGRAF != null)
+            {
+                return DALOgrenci.OgrenciEkle(p);
+            }
+            return -1;
+        }
+    }
+}
